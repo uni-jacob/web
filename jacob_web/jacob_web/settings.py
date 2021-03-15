de08f170b6807d1edbcc8a04d0518feca154831a
Daniil <dadyarri@gmail.com>
2021-03-16 00:01:04 +0300

    build(django): rotate secret key

--- jacob_web/jacob_web/settings.py
+++ jacob_web/jacob_web/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+%r#yd=@#ecyl4fxs5i@2jq*fa5r1ddqt637d+#90+-%^x$g8)'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
